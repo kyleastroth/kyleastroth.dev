9f11f9599a58dc4d727b44554b360e9dd25641e4
kyleastroth <kylastroth@icloud.com>
2024-08-15 18:04:24 -0700

    Added correct pdf links

--- src/config.ts
+++ src/config.ts
@@ -9,9 +9,17 @@ export const CONFIG = {
       featured: true,
       name: 'NeuroSLP',
       slug: 'neuro-slp',
-      description: 'Enhances LLVM\'s parameter selection for Superword Level Parallelism (SLP) using reinforcement learning. ',
+      description: 'Enhances LLVM\'s parameter selection for Superword Level Parallelism (SLP) using reinforcement learning.',
       image: 'https://drive.google.com/uc?export=view&id=1ZCGf1dDoSwNZfcLfoWOZnxzKpuQ_FgCx',
       link: 'https://github.com/kyleastroth/NeuroSlp',
+    },
+    {
+      featured: true,
+      name: 'Analyzing the Digital Discrimination of Internet Users in Cuba',
+      slug: 'cuba-embargo',
+      description: 'Quantified the extent & variety of methods by which global service providers implement embargo-based geoblocking in Cuba.',
+      image: 'https://drive.google.com/uc?export=view&id=1dC4CPELSU5HRY0mA_skYwoaEGYS4XVf_',
+      link: 'https://kyleastroth.github.io/cuba-geoblocking/docs/cuba_embargo_geoblocking.pdf'
     },
     {
       featured: true,
@@ -23,17 +31,9 @@ export const CONFIG = {
     },
     {
       featured: true,
-      name: 'Analyzing the Digital Discrimination of Internet Users in Cuba',
-      slug: 'cuba-embargo',
-      description: 'Quantified the extent & variety of methods by which global service providers implement embargo-based geoblocking in Cuba.',
-      image: 'https://drive.google.com/uc?export=view&id=1dC4CPELSU5HRY0mA_skYwoaEGYS4XVf_',
-      link: 'https://kyleastroth.github.io/transfer-learning/'
-    },
-    {
-      featured: true,
       name: 'Generative Adversarial Network',
       slug: 'gen-adversarial-network',
-      description: 'DCGAN trained and tuned on various image datasets to generate novel "fake" images. ',
+      description: 'DCGAN trained and tuned on various image datasets to generate novel "fake" images.',
       image: 'https://drive.google.com/uc?export=view&id=17XwyxClPRyPUfpFc-mOP4ix59GgX-A81',
       link: 'https://kastroth.github.io/gan/',
     },
@@ -43,7 +43,7 @@ export const CONFIG = {
       slug: 'qc-randomness-extractors',
       description: 'Defining quantum-to-classical (QC) and quantum-to-quantum (QQ) randomness extractors and exploring their cryptographic applications.',
       image: 'https://drive.google.com/uc?export=view&id=1ir5fH3bIr7hoDOW2llB8L_ZN58V8lgju',
-      link: 'https://kyleastroth.github.io/transfer-learning/',
+      link: 'https://kyleastroth.github.io/QC-randomness-extractors/docs/QuantumClassicalRandomnessExtractors.pdf',
     },
     {
       featured: false,
@@ -51,7 +51,7 @@ export const CONFIG = {
       slug: 'wasserstein-gan',
       description: 'Exploring Wasserstein Generative Adversarial Networks (WGAN) and their various applications including data augmentation for emotion recognition and singing voice synthesizers.',
       image: 'https://drive.google.com/uc?export=view&id=1qCJWTe2HpRkrkmWlEOX1eTDjPxny1KBH',
-      link: 'https://kyleastroth.github.io/transfer-learning/',
+      link: 'https://kyleastroth.github.io/wasserstein-gan/docs/WGAN.pdf',
     },
   ],
 };
